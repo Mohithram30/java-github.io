b5d3fba06c2c234903cbd21b9af6dc79f3246ef5
MOHITHRAM <mohithram266@gmail.com>
2025-03-14 19:43:37 +0530

    Add files via upload

---
+++ Inheritanace.java
@@ -0,0 +1,21 @@
+
+class A {
+    int one = 1;
+}
+
+class B extends A {
+    int two = 2;
+}
+
+class C extends B {
+    int three = 3;
+}
+
+public class Inheritanace {  
+    public static void main(String[] args) {
+        C ob1 = new C();
+        System.out.println(ob1.three);
+        System.out.println(ob1.two);
+        System.out.println(ob1.one);
+    }
+}
