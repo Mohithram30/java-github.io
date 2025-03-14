4a9de888e38964bc3a8941b8b481beb73549e797
MOHITHRAM <mohithram266@gmail.com>
2025-03-14 19:40:48 +0530

    Add files via upload

---
+++ Fueltank.java
@@ -0,0 +1,18 @@
+public class Fueltank
+{
+public static void main(String args[])
+{
+float capacity;
+public float addFuel(float newFuel)
+{
+ capacity=newFuel;
+return capacity;
+}
+
+public float consumeFuel(floatconsume)
+{
+capacity=consume;
+return capacity;
+}
+}
+}
