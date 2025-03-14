bd9701ed9052beff61b7064779e5bade621f2e78
MOHITHRAM <mohithram266@gmail.com>
2025-03-14 19:42:12 +0530

    Add files via upload

---
+++ Addition.java/Addition.java
@@ -0,0 +1,17 @@
+public class Addition
+{
+
+
+public void add(int a,int b,int c)
+{
+
+System.out.println("The value in a is :"+a);
+System.out.println("The value in b is :"+b);
+System.out.println("The value in c is :"+c);
+
+System.out.println("The sum is :"+(a+b+c));
+
+}
+
+
+}
---
+++ Addition.java/AdditionMain.java
@@ -0,0 +1,13 @@
+public class AdditionMain
+{
+
+	public static void main(String args [])
+	{
+		float f1=2.5f;
+		double d1=12.5;
+		int    n1=15;
+		System.out.println(f1+d1+n1);
+	}
+
+}
+ 
