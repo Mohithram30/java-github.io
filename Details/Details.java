7ed9d0ef627960d6b1a2bca3e36c0bfbf0c30a3e
MOHITHRAM <mohithram266@gmail.com>
2025-03-17 23:02:41 +0530

    Add files via upload

---
+++ Details/Details.java
@@ -0,0 +1,23 @@
+public class Details
+{
+String firstName;
+String lastName;
+int age;
+String PhoneNumber;
+String Email_Address;
+String DCob;
+String Gender;
+char initial;
+
+public Details(String fName,String lName,int age,String PNumber,String EAddress,String dob,String Ge,char initial)
+{
+firstName=fName;
+lastName=lName;
+this.age=age;
+PhoneNumber=PNumber;
+Email_Address=EAddress;
+Dob=dob;
+Gender=Ge;
+this.initial=initial;
+}
+}
---
+++ Details/DetailsMain.java
@@ -0,0 +1,87 @@
+public class DetailsMain
+{
+public static void main(String[] args)
+{
+ Details dt1=new Details("Mohith","Ram",23,"8344616148","mohithram266@gmail.com","2001/05/30","Male",'T');
+
+  System.out.println("Value in dt1 is:"+dt1);
+  
+   System.out.println("FirstName:"+dt1.firstName);
+   System.out.println("LastName:"+dt1.lastName);
+   System.out.println("Age:"+dt1.age);
+   System.out.println("PhoneNumber:"+dt1.PhoneNumber);
+   System.out.println("EmailAddres:"+dt1.Email_Address);
+   System.out.println("Dob:"+dt1.Dob);``
+   System.out.println("Gender:"+dt1.Gender);
+   System.out.println("initial is :"+dt1.initial);
+    
+
+
+Details dt2=new Details("vasanth","Kumar",23,"9677622302","vasanth2312@gmail.com","2001/03/02","Male",'v');
+
+
+
+  System.out.println("Value in dt2 is:"+dt2);
+  
+   System.out.println("FirstName:"+dt2.firstName);
+   System.out.println("LastName:"+dt2.lastName);
+   System.out.println("Age:"+dt2.age);
+   System.out.println("PhoneNumber:"+dt2.PhoneNumber);
+   System.out.println("EmailAddres:"+dt2.Email_Address);
+   System.out.println("Dob:"+dt2.Dob);
+   System.out.println("Gender:"+dt2.Gender);
+   System.out.println("initial is :"+dt2.initial);
+
+
+
+
+Details dt3=new Details("priyanka","Kannan",22,"7200098835","priyankakannan2002@gmail.com","2002/06/26","Female",'P');
+
+
+ System.out.println("Value in dt3 is:"+dt3);
+  
+   System.out.println("FirstName:"+dt3.firstName);
+   System.out.println("LastName:"+dt3.lastName);
+   System.out.println("Age:"+dt3.age);
+   System.out.println("PhoneNumber:"+dt3.PhoneNumber);
+   System.out.println("EmailAddres:"+dt3.Email_Address);
+   System.out.println("Dob:"+dt3.Dob);
+   System.out.println("Gender:"+dt3.Gender);
+   System.out.println("initial is :"+dt3.initial);
+
+
+
+
+Details dt4=new Details("Nimisha","Sabari",21,"8786773155","nimishasabari155@gmail.com","2004/02/15","Female",'N');
+
+
+ System.out.println("Value in dt4 is:"+dt4);
+  
+   System.out.println("FirstName:"+dt4.firstName);
+   System.out.println("LastName:"+dt4.lastName);
+   System.out.println("Age:"+dt4.age);
+   System.out.println("PhoneNumber:"+dt4.PhoneNumber);
+   System.out.println("EmailAddres:"+dt4.Email_Address);
+   System.out.println("Dob:"+dt4.Dob);
+   System.out.println("Gender:"+dt4.Gender);
+   System.out.println("initial is :"+dt4.initial);
+
+
+
+
+Details dt5=new Details("Madhumitha","Selvaraj",20,"8384593677","madhumithaselvaraj243@gmail.com","2004/03/24","Female",'M');
+  
+ System.out.println("Value in dt5 is:"+dt5);
+  
+   System.out.println("FirstName:"+dt5.firstName);
+   System.out.println("LastName:"+dt5.lastName);
+   System.out.println("Age:"+dt5.age);
+   System.out.println("PhoneNumber:"+dt5.PhoneNumber);
+   System.out.println("EmailAddres:"+dt5.Email_Address);
+   System.out.println("Dob:"+dt5.Dob);
+   System.out.println("Gender:"+dt5.Gender);
+   System.out.println("initial is :"+dt5.initial);
+
+
+}
+}
