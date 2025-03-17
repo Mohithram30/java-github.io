22d723ce9694291d7cc255beffbdf8deed560497
MOHITHRAM <mohithram266@gmail.com>
2025-03-17 23:18:31 +0530

    Add files via upload

---
+++ Student/Student.java
@@ -0,0 +1,22 @@
+public class Student
+{
+
+String firstName;
+String lastName;
+int age;
+String phoneNumber;
+char initial;
+
+public Student(String fname,String lName,int age,String pNumber,char initial)
+{
+
+firstName=fname;
+lastName=lName;
+this.age=age;
+phoneNumber=pNumber;
+this.initial=initial;
+
+
+}
+
+}
---
+++ Student/StudentMain.java
@@ -0,0 +1,35 @@
+public class StudentMain
+{
+
+public static void main(String[] args)
+{
+Student st1=new Student("Rajesh","Prakash",20,"9840654503",'T');
+
+System.out.println("Value in st1 is :"+st1);
+
+System.out.println("FirstName:"+st1.firstName);
+System.out.println("LastName:"+st1.lastName);
+System.out.println("Age:"+st1.age);
+System.out.println("PhoneNumber:"+st1.phoneNumber);
+System.out.println("Initial is :"+st1.initial);
+
+System.out.println("=========================================================");
+
+
+Student st2=new Student("Anand","Raghu",19,"9988877654",'K');
+
+System.out.println("Value in st2 is :"+st2);wh
+System.out.println("FirstName:"+st2.firstName);
+System.out.println("LastName:"+st2.lastName);
+System.out.println("Age:"+st2.age);
+System.out.println("PhoneNumber:"+st2.phoneNumber);
+System.out.println("Initial is :"+st2.initial);
+
+
+
+}
+
+
+
+
+}
